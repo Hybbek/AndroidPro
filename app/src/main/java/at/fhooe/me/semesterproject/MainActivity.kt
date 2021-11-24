3ba6da6126df49d496d6f0ebf4cee3b1e50cdf92
Marko Milosavljevic <milo.marko@icloud.com>
2021-11-25 00:22:43 +0100

    1. Checking for CountryCodes with exception 2. loading data 3. CountryActivity shows data now 4. Country FLAGS added

--- app/src/main/java/at/fhooe/me/semesterproject/MainActivity.kt
+++ app/src/main/java/at/fhooe/me/semesterproject/MainActivity.kt
@@ -3,16 +3,24 @@ package at.fhooe.me.semesterproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.amadeus.android.Amadeus
+import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class MainActivity : AppCompatActivity() {
 
+    val job = SupervisorJob()
+    val scope = CoroutineScope(Dispatchers.Main + job)
+    var country_code = "";
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,16 +29,37 @@ class MainActivity : AppCompatActivity() {
 
 
         findViewById<Button>(R.id.search_btn).setOnClickListener {
-            var app = application as AccessAPI;
 
             val getText = findViewById<EditText>(R.id.inputText)
-            app.country_code = getText.text.toString()
+            country_code = getText.text.toString()
 
-            app.get()
+            loadCountry(country_code)
+        }
+    }
 
-            val intent = Intent(this, CountryActivity::class.java)
-            startActivity(intent)
+    fun loadCountry(country_code: String) {
+        scope.launch {
+            try {
 
+                (application as AccessAPI).load(country_code)
+                startCountryActivity()
+
+            } catch (e: HttpException) {
+
+                sendToast("Invalid Country Code")
+            }
+
+        }
+    }
+
+    fun startCountryActivity() {
+        val intent = Intent(this, CountryActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun sendToast(msg: String) {
+        this.runOnUiThread {
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
         }
     }
 
